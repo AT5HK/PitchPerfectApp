6482d83281cfcef0884fc153907cf56652323b40
Michael Recachinas <mgr3yp@virginia.edu>
2015-07-20 18:39:29 -0400

    Complete RecordedAudio

--- PitchPerfect/RecordedAudio.swift
+++ PitchPerfect/RecordedAudio.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title:       String!
+    
+    init(title: String, filePathUrl: NSURL) {
+        self.title       = title
+        self.filePathUrl = filePathUrl
+    }
+}
