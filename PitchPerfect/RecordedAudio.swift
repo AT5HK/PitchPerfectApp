3e96a4981f6d219affe5a0873740fe9cf37e027f
Michael Recachinas <mgr3yp@virginia.edu>
2015-07-20 18:23:15 -0400

    Add gitignore to repo

---
+++ PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,9 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Michael Recachinas on 7/12/15.
+//  Copyright (c) 2015 Michael Recachinas. All rights reserved.
+//
+
+import Foundation
