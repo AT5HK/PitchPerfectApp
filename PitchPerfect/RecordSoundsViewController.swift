0cffc39f480e5d48bf132a9e1278e27b3f64202a
Michael Recachinas <mgr3yp@virginia.edu>
2015-07-20 21:41:51 -0400

    Fix code formatting and abstract stop recording logic

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -12,15 +12,14 @@ import AVFoundation
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     @IBOutlet weak var recordingInProgress: UILabel!
-    @IBOutlet weak var stopButton:          UIButton!
-    @IBOutlet weak var recordButton:        UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     var audioRecorder: AVAudioRecorder!
     var recordedAudio: RecordedAudio!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -31,36 +30,34 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func recordAudio(sender: UIButton) {
-        recordButton.enabled     = false
+        recordButton.enabled = false
         recordingInProgress.text = "Recording in Progress"
-        stopButton.hidden        = false
+        stopButton.hidden = false
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
-        
-//        let currentDateTime = NSDate()
-//        let formatter = NSDateFormatter()
-//        formatter.dateFormat = "ddMMyyyy-HHmmss"
-//        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
         let recordingName = "my_audio.wav"
-        let pathArray     = [dirPath, recordingName]
-        let filePath      = NSURL.fileURLWithPathComponents(pathArray)
-//        println(filePath)
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
         
         var session = AVAudioSession.sharedInstance()
         session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
         
         audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
         
-        audioRecorder.delegate        = self
+        audioRecorder.delegate = self
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
     }
 
+    func audioStopped() {
+        recordButton.enabled = true
+        stopButton.hidden = true
+    }
+    
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
 
         if flag {
@@ -68,9 +65,8 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
             self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
         } else {
-            println("Recording not successful")
-            recordButton.enabled = true
-            stopButton.hidden    = true
+            recordingInProgress.text = "Recording Not Successful"
+            audioStopped()
         }
     }
     
@@ -83,10 +79,8 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     @IBAction func stopRecording(sender: UIButton) {
-        recordButton.enabled     = true
         recordingInProgress.text = "Tap to Record"
-        stopButton.hidden        = true
-        
+        audioStopped()
         audioRecorder.stop()
         var audioSession = AVAudioSession.sharedInstance()
         audioSession.setActive(false, error: nil)
