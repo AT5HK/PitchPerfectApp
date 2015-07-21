9610ec939acd86beed948bea2d107705ce4cbce5
Michael Recachinas <mgr3yp@virginia.edu>
2015-07-20 22:19:29 -0400

    Fix code styling

--- PitchPerfect/RecordedAudio.swift
+++ PitchPerfect/RecordedAudio.swift
@@ -10,10 +10,10 @@ import Foundation
 
 class RecordedAudio: NSObject{
     var filePathUrl: NSURL!
-    var title:       String!
+    var title: String!
     
     init(title: String, filePathUrl: NSURL) {
-        self.title       = title
+        self.title = title
         self.filePathUrl = filePathUrl
     }
 }
